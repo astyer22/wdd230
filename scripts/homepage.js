88685f83a9229ac4e931e88f1fc85ceea842657f
astyer22 <astyer@byui.edu>
2024-01-24 22:46:57 -0700

    push

---
+++ scripts/homepage.js
@@ -0,0 +1,8 @@
+// Hamburger Menu script
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('show');
+	hamButton.classList.toggle('open');
+});
