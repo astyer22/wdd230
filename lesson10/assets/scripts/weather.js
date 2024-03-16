5df01fb3e8a7c3a9f88563646d553d25c138a340
astyer22 <astyer@byui.edu>
2024-03-15 20:05:04 -0600

    typo fixed

--- lesson10/assets/scripts/weather.js
+++ lesson10/assets/scripts/weather.js
@@ -1,18 +1,18 @@
 const key = 'a99c4008254a2507bc15cbf9d86d76d6';
-const url=`https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=${key}&units=imperial`
+const url=`https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=${key}&units=imperial`;
 
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weatherr-icon');
+const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 function displayResults(data) {
-  let temp = Math.round(data.main.temp);
-  currentTemp.innerHTML = `${temp}&deg;F`;
-  let descrpt = data.weather[0].description.toUpperCase();
-  captionDesc.textContent = `${descrpt}`;
-  weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-  weatherIcon.setAttribute('alt', `${descrpt}`);
-  weatherIcon.setAttribute('loading', 'lazy');
+    let temp = Math.round(data.main.temp);
+    currentTemp.innerHTML = `${temp}&deg;F`;
+    let descrpt = data.weather[0].description.toUpperCase();
+    captionDesc.textContent = `${descrpt}`;
+    weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    weatherIcon.setAttribute('alt', `${descrpt}`);
+    weatherIcon.setAttribute('loading', 'lazy');
 };
 
 async function apiFetch() {
@@ -20,6 +20,7 @@ async function apiFetch() {
         const response = await fetch(url)
         if (response.ok) {
             const data = await response.json();
+            displayResults(data);
             console.log(data); 
         } else {
             throw Error (await response.text());
