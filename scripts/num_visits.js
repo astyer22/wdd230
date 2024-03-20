4c5ee8f4bacf7c68d73d51e184189fbb605742e6
astyer22 <astyer@byui.edu>
2024-03-19 21:15:51 -0600

    push

--- scripts/num_visits.js
+++ scripts/num_visits.js
@@ -3,7 +3,7 @@ const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 if (numVisits !== 0) {
-    visitsDisplay.textContent = console.log(numVisits);
+    visitsDisplay.textContent = numVisits;
 } else {
     visitsDisplay.textContent = `This is your first visit, welcome!`;
 }
